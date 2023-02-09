348bc86255dddf255a3792b69801f3976c6853fe
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-02-09 22:49:56 +0700

    feat: font setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif']
+      }
+    },
 	},
 	plugins: [],
 }
